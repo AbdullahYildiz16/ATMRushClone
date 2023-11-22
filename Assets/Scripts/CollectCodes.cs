42b6565294cb95ccfb38b008464612c09cef10b7
AbdullahY <yldzabdullah2003@gmail.com>
2023-11-22 12:11:30 +0300

    feat: Init the project

---
+++ Assets/Scripts/CollectCodes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCodes : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collect"))
+        {
+            other.gameObject.transform.position = transform.position + Vector3.forward;
+            other.gameObject.AddComponent<CollectCodes>();
+            Destroy(gameObject.GetComponent<CollectCodes>());
+            other.GetComponent<BoxCollider>().isTrigger = false;
+            other.gameObject.AddComponent<NodeMovement>();
+            other.gameObject.GetComponent<NodeMovement>().connectedNode = transform;
+            other.gameObject.tag = "Collected";
+        }
+    }
+}
---
+++ Assets/Scripts/NodeMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeMovement : MonoBehaviour
+{
+    public Transform connectedNode;
+    public float nodeSpeed = 20f;
+    private void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, connectedNode.transform.position.x, nodeSpeed*Time.deltaTime),
+            connectedNode.transform.position.y,
+            connectedNode.position.z +0.5f);
+    }
+}
